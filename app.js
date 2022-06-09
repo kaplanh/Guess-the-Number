0fe88f1a67f16cab20eb228e1a98fbcad039ef8e
kplnhsyn <kplnhsyn.49@gmail.com>
2022-06-09 13:13:49 +0200

    fix

--- app.js
+++ app.js
@@ -1,60 +1,72 @@
-let random = Math.floor(Math.random() * 100 + 1);
+let random = Math.floor(Math.random() * 1000 + 1);
 let input = document.querySelector(".input");
 let check = document.querySelector(".check");
 let restart = document.querySelector(".restart");
 let paragraf = document.querySelector(".para");
 let please = document.querySelector(".please");
 let lifepar = document.querySelector(".life");
-let life = 7;
+let life = 10;
 
 const guessGame = function () {
-  if (life > 1) {
-    if (input.value > 100 || input.value < 0) {
+  if (life == 1) {
+    if (input.value == random) {
+      paragraf.innerHTML = "👏 CONGRATULATIONS! 👏";
+      check.style.display = "none";
+      input.style.display = "none";
+      restart.style.display = "block";
+      // applause.play();
+    } else {
+      paragraf.innerHTML = "😢 Game Over 😢";
+      check.style.display = "none";
+      input.style.display = "none";
+      restart.style.display = "block";
+      please.innerHTML = "";
+      lifepar.innerHTML = "👆👆👆";
+      lifepar.style.textShadow = "none";
+      life--;
+      // loser.play();
+    }
+  } else {
+    if (input.value > 1000 || input.value < 0 || input.value == "") {
       paragraf.innerText = "Please enter a valid number";
     } else {
       if (input.value > random) {
         paragraf.innerHTML = "Lower number please";
         life--;
         document.querySelector(".last").innerText = input.value;
-        down.play();
+        // down.play();
       } else if (input.value < random) {
         paragraf.innerHTML = "Greater number please";
         life--;
         document.querySelector(".first").innerText = input.value;
-        up.play();
+        // up.play();
       } else if (input.value == random) {
         paragraf.innerHTML = "👏 CONGRATULATIONS! 👏";
-        life--;
         check.style.display = "none";
+        input.style.display = "none";
         restart.style.display = "block";
-        applause.play();
+        // applause.play();
       }
     }
-  } else {
-    paragraf.innerHTML = "😢 Game Over 😢";
-    check.style.display = "none";
-    input.style.display = "none";
-    restart.style.display = "block";
-    please.innerHTML = "";
-    lifepar.innerHTML = "👆👆👆";
-    lifepar.style.textShadow = "none";
-
-    life--;
-    loser.play();
+    
   }
+
   document.querySelector(".life-span").innerHTML = life;
   input.value = "";
-};
+}
+  check.addEventListener("click", guessGame);
 
-check.addEventListener("click", guessGame);
-
-input.addEventListener("keydown", (e) => {
+  input.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
-    check.click();
+   
     guessGame();
   }
-});
+
+  });
+
 
 window.onload = () => {
+  fon.play();
   input.focus();
 };
+
